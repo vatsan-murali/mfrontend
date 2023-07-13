35db6640924f438106bf86549be8d33fa7c973d6
Vatsan <118588346+vatsan-murali@users.noreply.github.com>
2023-07-13 12:19:29 +0530

    Update ADM.js

--- src/components/ADM.js
+++ src/components/ADM.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ADM = () => {
   const [selectedImage, setSelectedImage] = useState(null);
